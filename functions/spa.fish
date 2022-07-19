54e916ae504168d7d0062c1e8729814af2e11f9a
Dang <huynhminhdang@gmail.com>
2022-07-20 06:44:55 +0700

    Create spa.fish

---
+++ functions/spa.fish
@@ -0,0 +1,8 @@
+function spa --argument env
+  test web = (basename $PWD) || return
+  test -f .env || test -n "$env" || set -l env (string lower -- (string replace --regex '.*@' '' -- $AWS_PROFILE))
+  cp ~/.config/env/$env .env
+  test -f ~/.config/env/global && cat ~/.config/env/global >> .env
+  test -d node_modules || npm i
+  ./node_modules/.bin/nuxt spa
+end

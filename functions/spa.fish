bdd91d6abaf4685d8f9daaef944a7d01385e4654
Dang <huynhminhdang@gmail.com>
2022-07-20 15:55:45 +0700

    Update spa.fish

--- functions/spa.fish
+++ functions/spa.fish
@@ -1,8 +1,10 @@
 function spa --argument env
   test web = (basename $PWD) || return
-  test -f .env || test -n "$env" || set -l env (string lower -- (string replace --regex '.*@' '' -- $AWS_PROFILE))
-  cp ~/.config/env/$env .env
-  test -f ~/.config/env/global && cat ~/.config/env/global >> .env
+  if test -z "$env" -a ! -f .env
+    set env (string lower -- (string replace --regex '.*@' '' -- $AWS_PROFILE))
+    cp ~/.config/env/$env .env
+    test -f ~/.config/env/global && cat ~/.config/env/global >> .env
+  end    
   test -d node_modules || npm i
   ./node_modules/.bin/nuxt --spa
 end
